00fd67a161cf373d8ca51eab09fbb54893e20ea6
Mina Rezkalla <minashakeralfy@gmail.com>
2020-08-05 13:22:55 +0200

    add user domain model

---
+++ src/main/java/com/mina/users_registration_form/model/User.java
@@ -0,0 +1,24 @@
+package com.mina.users_registration_form.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private int age;
+    private String gender;
+    private String city;
+    private String street;
+    private String postcode;
+
+
+}
