cd48182b5fd47aeb9deab2add6051375dcf6a73b
Mina Rezkalla <minashakeralfy@gmail.com>
2020-08-05 11:49:30 +0200

    first commit

---
+++ src/main/java/com/mina/users_registration_form/UsersRegistrationFormApplication.java
@@ -0,0 +1,13 @@
+package com.mina.users_registration_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRegistrationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersRegistrationFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mina/users_registration_form/UsersRegistrationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mina.users_registration_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersRegistrationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
